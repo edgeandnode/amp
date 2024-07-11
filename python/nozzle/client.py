3f6f15e353dabbad7d1b814bc7a7e323a3a36392
“Vivian <yp2457@columbia.edu>
2024-07-11 14:13:53 -0400

    indexer

--- python/nozzle/client.py
+++ python/nozzle/client.py
@@ -2,6 +2,7 @@ from pyarrow import flight
 from google.protobuf.any_pb2 import Any
 
 from . import FlightSql_pb2
+import pandas as pd
 
 class Client:
     def __init__(self, url):
@@ -13,26 +14,29 @@ class Client:
     # If `read_all` is `False`, returns a generator of `pyarrow.RecordBatch`. This is suitable for
     # streaming larger result sets.
     def get_sql(self, query, read_all=False):
-        # Create a CommandStatementQuery message
-        command_query = FlightSql_pb2.CommandStatementQuery()
-        command_query.query = query
+        try:
+            # Create a CommandStatementQuery message
+            command_query = FlightSql_pb2.CommandStatementQuery()
+            command_query.query = query
 
 
-        # Wrap the CommandStatementQuery in an Any type
-        any_command = Any()
-        any_command.Pack(command_query)
-        cmd = any_command.SerializeToString()
+            # Wrap the CommandStatementQuery in an Any type
+            any_command = Any()
+            any_command.Pack(command_query)
+            cmd = any_command.SerializeToString()
 
 
-        flight_descriptor = flight.FlightDescriptor.for_command(cmd)
-        info = self.conn.get_flight_info(flight_descriptor)
-        reader = self.conn.do_get(info.endpoints[0].ticket)
+            flight_descriptor = flight.FlightDescriptor.for_command(cmd)
+            info = self.conn.get_flight_info(flight_descriptor)
+            reader = self.conn.do_get(info.endpoints[0].ticket)
 
-        if read_all:
-            return reader.read_all()
-        else:
-            return self._batch_generator(reader)
-
+            if read_all:
+                return reader.read_all()
+            else:
+                return self._batch_generator(reader)
+        except Exception as e:
+            print(f"Error executing query: {query}\n{e}")
+            raise
     def _batch_generator(self, reader):
         while True:
             try:
