7d7b1ebc90c18ff757be3bd83ff8c26dcf555dc4
Theo Butler <theodusbutler@gmail.com>
2024-09-13 10:48:26 -0400

    add tests for evm-rpc

--- tests/src/tests.rs
+++ tests/src/tests.rs
@@ -3,6 +3,21 @@ use common::tracing;
 use log::warn;
 
 #[tokio::test]
+async fn evm_rpc_single() {
+    tracing::register_logger();
+
+    if std::env::var("EVM_RPC_TESTS_BLESS").is_ok() {
+        bless("eth_rpc", 15_000_000, 15_000_000).await.unwrap();
+
+        warn!("bless completed successfully, running check blocks");
+    }
+
+    check_blocks("eth_rpc", 15_000_000, 15_000_000)
+        .await
+        .unwrap();
+}
+
+#[tokio::test]
 async fn eth_firehose_single() {
     tracing::register_logger();
 
