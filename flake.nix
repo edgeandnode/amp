3e89547c86c670e3de9af8d2f05e256223352c2e
Sebastian Lorenz <fubhy@fubhy.com>
2024-09-25 09:38:19 +0200

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+  outputs = { self, nixpkgs, rust-overlay }:
+    let
+      overlays = [
+        (import rust-overlay)
+        (self: super: {
+          rustToolchain = super.rust-bin.stable.latest.default;
+        })
+      ];
+
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit overlays system; };
+      });
+    in
+    {
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = (with pkgs; [
+            # Includes cargo, clippy, cargo-fmt, rustdoc, rustfmt, and other tools.
+            rustToolchain
+            protobuf
+            cmake
+          ]) ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [
+            libiconv
+            # Required to compile `dump`.
+            darwin.apple_sdk.frameworks.SystemConfiguration
+          ]);
+        };
+      });
+    };
+}
