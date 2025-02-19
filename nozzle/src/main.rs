0a2798182796b26c2faef10d39579747b0e65490
Theo Butler <theodusbutler@gmail.com>
2025-02-19 10:23:03 -0500

    refactor: nozzle dump module

--- nozzle/src/main.rs
+++ nozzle/src/main.rs
@@ -1,19 +1,13 @@
-use std::collections::{BTreeMap, BTreeSet};
-use std::time::Duration;
+mod dump;
+
 use std::{net::SocketAddr, sync::Arc};
 
 use arrow_flight::flight_service_server::FlightServiceServer;
 use clap::Parser as _;
-use common::manifest;
 use common::{config::Config, tracing, BoxError};
-use datafusion::catalog_common::resolve_table_references;
-use datafusion::parquet;
-use dataset_store::{sql_datasets, DatasetStore};
 use futures::{StreamExt as _, TryStreamExt as _};
 use log::info;
 use metadata_db::MetadataDb;
-use parquet::basic::Compression;
-use parquet::basic::ZstdLevel;
 use server::service::Service;
 use tonic::transport::Server;
 
@@ -98,10 +92,9 @@ async fn main_inner() -> Result<(), BoxError> {
     let config = Arc::new(
         Config::load(args.config, true, None).map_err(|e| format!("failed to load config: {e}"))?,
     );
-    let metadata_db = if let Some(url) = &config.metadata_db_url {
-        Some(MetadataDb::connect(url).await?)
-    } else {
-        None
+    let metadata_db = match &config.metadata_db_url {
+        Some(url) => Some(MetadataDb::connect(url).await?),
+        None => None,
     };
 
     match args.command {
@@ -111,67 +104,23 @@ async fn main_inner() -> Result<(), BoxError> {
             n_jobs,
             partition_size_mb,
             disable_compression,
-            dataset: mut datasets,
+            dataset: datasets,
             ignore_deps,
             run_every_mins,
         } => {
-            let dataset_store = DatasetStore::new(config.clone(), metadata_db.clone());
-            let partition_size = partition_size_mb * 1024 * 1024;
-            let compression = if disable_compression {
-                parquet::basic::Compression::UNCOMPRESSED
-            } else {
-                Compression::ZSTD(ZstdLevel::try_new(1).unwrap())
-            };
-            let parquet_opts = dump::parquet_opts(compression, true);
-            let end_block = end_block.map(|e| resolve_end_block(start, e)).transpose()?;
-            let run_every =
-                run_every_mins.map(|s| tokio::time::interval(Duration::from_secs(s * 60)));
-
-            if !ignore_deps {
-                datasets = datasets_and_dependencies(&dataset_store, datasets).await?;
-            }
-
-            let dump_order: Vec<&str> = datasets.iter().map(|d| d.as_str()).collect();
-            info!("dump order: {}", dump_order.join(", "));
-
-            match run_every {
-                None => {
-                    for dataset_name in datasets {
-                        dump::dump_dataset(
-                            &dataset_name,
-                            &dataset_store,
-                            &config,
-                            metadata_db.as_ref(),
-                            n_jobs,
-                            partition_size,
-                            &parquet_opts,
-                            start,
-                            end_block,
-                        )
-                        .await?
-                    }
-                }
-                Some(mut run_every) => loop {
-                    run_every.tick().await;
-
-                    for dataset_name in &datasets {
-                        dump::dump_dataset(
-                            dataset_name,
-                            &dataset_store,
-                            &config,
-                            metadata_db.as_ref(),
-                            n_jobs,
-                            partition_size,
-                            &parquet_opts,
-                            start,
-                            end_block,
-                        )
-                        .await?;
-                    }
-                },
-            }
-
-            Ok(())
+            dump::dump(
+                config,
+                metadata_db,
+                datasets,
+                ignore_deps,
+                start,
+                end_block,
+                n_jobs,
+                partition_size_mb,
+                disable_compression,
+                run_every_mins,
+            )
+            .await
         }
         Command::Server => {
             info!("memory limit is {} MB", config.max_mem_mb);
@@ -265,170 +214,4 @@ async fn handle_jsonl_request(
         .header("content-type", "application/x-ndjson")
         .body(axum::body::Body::from_stream(stream))
         .unwrap()
-}
-
-/// Return the input datasets and their dataset dependencies. The output set is ordered such that
-/// each dataset comes after all datasets it depends on.
-async fn datasets_and_dependencies(
-    store: &Arc<DatasetStore>,
-    mut datasets: Vec<String>,
-) -> Result<Vec<String>, BoxError> {
-    let mut deps: BTreeMap<String, Vec<String>> = Default::default();
-    while !datasets.is_empty() {
-        let dataset = store.load_dataset(&datasets.pop().unwrap()).await?;
-        let sql_dataset = match dataset.kind.as_str() {
-            sql_datasets::DATASET_KIND => store.load_sql_dataset(&dataset.name).await?,
-            manifest::DATASET_KIND => store.load_manifest_dataset(&dataset.name).await?,
-            _ => {
-                deps.insert(dataset.name.clone(), vec![]);
-                continue;
-            }
-        };
-        let mut refs: Vec<String> = Default::default();
-        for query in sql_dataset.queries.values() {
-            let (tables, _) = resolve_table_references(query, true)?;
-            refs.append(
-                &mut tables
-                    .iter()
-                    .filter_map(|t| t.schema())
-                    .map(ToString::to_string)
-                    .collect(),
-            );
-        }
-        let mut untracked_refs = refs
-            .iter()
-            .filter(|r| deps.keys().all(|d| d != *r))
-            .cloned()
-            .collect();
-        datasets.append(&mut untracked_refs);
-        deps.insert(dataset.name, refs);
-    }
-
-    dependency_sort(deps)
-}
-
-/// Given a map of values to their dependencies, return a set where each value is ordered after
-/// all of its dependencies. An error is returned if a cycle is detected.
-fn dependency_sort(deps: BTreeMap<String, Vec<String>>) -> Result<Vec<String>, BoxError> {
-    let nodes: BTreeSet<&String> = deps
-        .iter()
-        .flat_map(|(ds, deps)| std::iter::once(ds).chain(deps))
-        .collect();
-    let mut ordered: Vec<String> = Default::default();
-    let mut visited: BTreeSet<&String> = Default::default();
-    let mut visited_cycle: BTreeSet<&String> = Default::default();
-    fn dfs<'a>(
-        node: &'a String,
-        deps: &'a BTreeMap<String, Vec<String>>,
-        ordered: &mut Vec<String>,
-        visited: &mut BTreeSet<&'a String>,
-        visited_cycle: &mut BTreeSet<&'a String>,
-    ) -> Result<(), BoxError> {
-        if visited_cycle.contains(node) {
-            return Err(format!("dependency cycle detected on dataset {node}").into());
-        }
-        if visited.contains(node) {
-            return Ok(());
-        }
-        visited_cycle.insert(node);
-        for dep in deps.get(node).into_iter().flatten() {
-            dfs(dep, deps, ordered, visited, visited_cycle)?;
-        }
-        visited_cycle.remove(node);
-        visited.insert(node);
-        ordered.push(node.to_string());
-        Ok(())
-    }
-    for node in nodes {
-        if !visited.contains(node) {
-            dfs(node, &deps, &mut ordered, &mut visited, &mut visited_cycle)?;
-        }
-    }
-    Ok(ordered)
-}
-
-// if end_block starts with "+" then it is a relative block number
-// otherwise, it's an absolute block number and should be after start_block
-fn resolve_end_block(start_block: u64, end_block: String) -> Result<u64, BoxError> {
-    let end_block = if end_block.starts_with('+') {
-        let relative_block = end_block
-            .trim_start_matches('+')
-            .parse::<u64>()
-            .map_err(|e| format!("invalid relative end block: {e}"))?;
-        start_block + relative_block
-    } else {
-        end_block
-            .parse::<u64>()
-            .map_err(|e| format!("invalid end block: {e}"))?
-    };
-    if end_block < start_block {
-        return Err("end_block must be greater than or equal to start_block".into());
-    }
-    if end_block == 0 {
-        return Err("end_block must be greater than 0".into());
-    }
-    Ok(end_block)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_resolve_block_range() {
-        let test_cases = vec![
-            (10, "20", Ok(20)),
-            (0, "1", Ok(1)),
-            (
-                18446744073709551614,
-                "18446744073709551615",
-                Ok(18_446_744_073_709_551_615u64),
-            ),
-            (10, "+5", Ok(15)),
-            (100, "90", Err(BoxError::from(""))),
-            (0, "0", Err(BoxError::from(""))),
-            (0, "0x", Err(BoxError::from(""))),
-            (0, "xxx", Err(BoxError::from(""))),
-            (100, "+1000x", Err(BoxError::from(""))),
-            (100, "+1x", Err(BoxError::from(""))),
-        ];
-
-        for (start_block, end_block, expected) in test_cases {
-            match resolve_end_block(start_block, end_block.into()) {
-                Ok(result) => assert_eq!(expected.unwrap(), result),
-                Err(_) => assert!(expected.is_err()),
-            }
-        }
-    }
-
-    #[test]
-    fn dependency_sort_order() {
-        #[allow(clippy::type_complexity)]
-        let cases: &[(&[(&str, &[&str])], Option<&[&str]>)] = &[
-            (&[("a", &["b"]), ("b", &["a"])], None),
-            (&[("a", &["b"])], Some(&["b", "a"])),
-            (&[("a", &["b", "c"])], Some(&["b", "c", "a"])),
-            (&[("a", &["b"]), ("c", &[])], Some(&["b", "a", "c"])),
-            (&[("a", &["b"]), ("c", &["b"])], Some(&["b", "a", "c"])),
-            (
-                &[("a", &["b", "c"]), ("b", &["d"]), ("c", &["d"])],
-                Some(&["d", "b", "c", "a"]),
-            ),
-            (
-                &[("a", &["b", "c"]), ("b", &["c", "d"])],
-                Some(&["c", "d", "b", "a"]),
-            ),
-        ];
-        for (input, expected) in cases {
-            let deps = input
-                .iter()
-                .map(|(k, v)| (k.to_string(), v.iter().map(ToString::to_string).collect()))
-                .collect();
-            let result = dependency_sort(deps);
-            match expected {
-                Some(expected) => assert_eq!(*expected, result.unwrap()),
-                None => assert!(result.is_err()),
-            }
-        }
-    }
 }
