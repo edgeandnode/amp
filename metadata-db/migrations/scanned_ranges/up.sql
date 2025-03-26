bfd3609fe75ae50586242d1394d0241fc7a18579
John Swanson <jswanson@edgeandnode.com>
2025-03-26 15:35:59 -0700

    including additional index on scanned_ranges
    Signed-off-by: John Swanson <jswanson@edgeandnode.com>

--- metadata-db/migrations/scanned_ranges/up.sql
+++ metadata-db/migrations/scanned_ranges/up.sql
@@ -5,5 +5,8 @@ CREATE TABLE scanned_ranges (
     metadata JSONB NOT NULL
 );
 
+CREATE UNIQUE INDEX unique_file_name_per_location_id
+ON scanned_ranges (location_id, file_name);
+
 CREATE UNIQUE INDEX unique_range_boundaries_per_dataset_version_table
 ON scanned_ranges (location_id, (metadata->>'range_start')::BIGINT, (metadata->>'range_end')::BIGINT);
