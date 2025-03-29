3acf9d50c058fba3efc9333942ad04c12e64f9df
Ford <ford@thegraph.com>
2025-03-28 17:01:25 -0700

    python: add `altair` dependency for charting, use in example notebook

--- python/notebooks/marimo_example_nb.py
+++ python/notebooks/marimo_example_nb.py
@@ -6,9 +6,10 @@ app = marimo.App(width='medium')
 
 @app.cell
 def _():
+    import altair as alt
     import marimo as mo
 
-    return (mo,)
+    return alt, mo
 
 
 @app.cell
@@ -41,6 +42,36 @@ def _(client):
 @app.cell
 def _(block_count):
     block_count
+    return
+
+
+@app.cell
+def _(client):
+    blocks_per_hour = client.get_sql("select date_bin(interval '1 day', timestamp) as day, count(*) as blocks_per_day from eth_firehose.blocks group by date_bin(interval '1 day', timestamp) having count(*) > 1", read_all=True)
+    blocks_df = blocks_per_hour.to_pandas()
+    return blocks_df, blocks_per_hour
+
+
+@app.cell
+def _(alt, blocks_df, mo):
+    day_range_min_1 = blocks_df['day'].min()
+    day_range_max_1 = blocks_df['day'].max()
+
+    chart = mo.ui.altair_chart(
+        alt.Chart(blocks_df)
+        .mark_point()
+        .encode(
+            alt.X('day', scale=alt.Scale(domain=(day_range_min_1, day_range_max_1))),
+            y='blocks_per_day',
+            color='blocks_per_day',
+        )
+    )
+    return chart, day_range_max_1, day_range_min_1
+
+
+@app.cell
+def _(chart, mo):
+    mo.vstack([chart, mo.ui.table(chart.value)])
     return
 
 
