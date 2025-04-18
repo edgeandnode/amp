6d08c28e06b43dc2569426639ae59bf5c4823fdc
Sebastian Lorenz <fubhy@fubhy.com>
2025-04-18 15:42:43 +0200

    jsonl streaming client

--- typescript/nozzle/src/index.ts
+++ typescript/nozzle/src/index.ts
@@ -3,6 +3,7 @@ import * as Model from "./Model.js";
 
 export * as Model from "./Model.js";
 export * as Api from "./Api.js";
+export * as JsonLines from "./JsonLines.js";
 export * as ConfigLoader from "./ConfigLoader.js";
 export * as ManifestLoader from "./ManifestLoader.js";
 export * as ManifestBuilder from "./ManifestBuilder.js";
