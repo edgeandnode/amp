48cfd9cd46aa9eef946d82b5ff0a2103c05bdafe
Sebastian Lorenz <fubhy@fubhy.com>
2025-04-19 12:05:14 +0200

    ability to limit query output and choose format (#153)

--- typescript/nozzle/src/cli/commands/query.ts
+++ typescript/nozzle/src/cli/commands/query.ts
@@ -1,23 +1,46 @@
-import { Args, Command } from "@effect/cli";
-import { Console, Effect, Schema } from "effect";
+import { Args, Command, Options } from "@effect/cli";
+import { Console, Effect, Match, Option, Schema, Stream } from "effect";
 import * as ArrowFlight from "../../ArrowFlight.js";
 
 export const query = Command.make("query", {
   args: {
     query: Args.text({ name: "query" }).pipe(
-      Args.withDescription("The SQL query string"),
+      Args.withDescription("The SQL query"),
     ),
-    limit: Args.integer({ name: "limit" }).pipe(
-      Args.withDefault(10),
-      Args.withDescription("The number of rows to return"),
+    limit: Options.integer("limit").pipe(
+      Options.optional,
+      Options.withDescription("The number of rows to return"),
     ),
-  }
+    format: Options.choice("format", ["table", "json", "pretty"]).pipe(
+      Options.withDefault("table"),
+      Options.withDescription("The format to output the results in"),
+    ),
+  },
 }, ({ args }) => Effect.gen(function* () {
   const flight = yield* ArrowFlight.ArrowFlight;
-  const table = yield* flight.table(args.query);
-  const encode = Schema.encodeUnknownSync(ArrowFlight.generateSchema(table.schema));
-  yield* Console.table([...table].map((_) => encode(_)));
+  const table = yield* flight.table(args.query).pipe(Effect.map((table) => Option.match(args.limit, {
+    onSome: (_) => table.slice(0, _),
+    onNone: () => table,
+  })));
+
+  const schema = ArrowFlight.generateSchema(table.schema);
+  yield* Match.value(args.format).pipe(
+    Match.when("json", () => Effect.succeed([...table]).pipe(
+      Effect.flatMap(Schema.encodeUnknown(Schema.Array(schema))),
+      Effect.map((_) => JSON.stringify(_, null, 2)),
+      Effect.flatMap(Console.log),
+    )),
+    Match.when("table", () => Effect.succeed([...table]).pipe(  
+      Effect.flatMap(Schema.encodeUnknown(Schema.Array(schema))),
+      Effect.flatMap(Console.table),
+    )),
+    Match.when("pretty", () => Stream.fromIterable([...table]).pipe(
+      Stream.mapEffect(Schema.encodeUnknown(schema)),
+      Stream.runForEach(Console.log),
+    )),
+    Match.exhaustive,
+  );
 })).pipe(
-  Command.withDescription("Run a Nozzle SQL query"),
+  Command.withDescription("Perform a Nozzle SQL query"),
   Command.provide(ArrowFlight.ArrowFlight.Default),
 );
