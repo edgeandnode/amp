4916f8f3b195116d24b6b9ce891a14d40ed2ca54
Sebastian Lorenz <fubhy@fubhy.com>
2025-04-19 16:55:25 +0200

    make arrow flight layer creation modular

--- typescript/nozzle/src/ArrowFlight.ts
+++ typescript/nozzle/src/ArrowFlight.ts
@@ -1,91 +1,108 @@
 import { create, toBinary } from "@bufbuild/protobuf"
 import { anyPack, AnySchema } from "@bufbuild/protobuf/wkt"
+import type { Transport } from "@connectrpc/connect"
 import { createClient } from "@connectrpc/connect"
-import { createGrpcTransport } from "@connectrpc/connect-node"
+import { createConnectTransport, createGrpcTransport } from "@connectrpc/connect-node"
 import { FetchHttpClient } from "@effect/platform"
 import type { DataType, Field as ArrowField, Schema as ArrowSchema, TypeMap } from "apache-arrow"
 import { RecordBatchReader, Table, Type } from "apache-arrow"
-import { Chunk, Config, Data, DateTime, Effect, Option, Schema, Stream, type Types } from "effect"
+import { Chunk, Config, Data, DateTime, Effect, Layer, Option, Schema, Stream, type Types } from "effect"
 import * as Proto from "./Proto.js"
 
 export class ArrowFlightError extends Data.TaggedError("ArrowFlightError")<{
   cause: unknown
 }> {}
 
+const make = (transport: Transport) => {
+  const client = createClient(Proto.Flight.FlightService, transport)
+  const stream = (query: string) =>
+    Effect.gen(function*() {
+      const cmd = create(Proto.FlightSql.CommandStatementQuerySchema, { query })
+      // TODO: Why is it necessary to pack the command into an Any?
+      const any = anyPack(Proto.FlightSql.CommandStatementQuerySchema, cmd)
+      const descriptor = create(Proto.Flight.FlightDescriptorSchema, {
+        type: Proto.Flight.FlightDescriptor_DescriptorType.CMD,
+        cmd: toBinary(AnySchema, any)
+      })
+
+      const info = yield* Effect.tryPromise({
+        try: (_) => client.getFlightInfo(descriptor, { signal: _ }),
+        catch: (cause) => new ArrowFlightError({ cause })
+      })
+
+      const ticket = yield* Option.fromNullable(info.endpoint[0]?.ticket).pipe(Option.match({
+        onNone: () => new ArrowFlightError({ cause: new Error("No flight ticket found") }),
+        onSome: (ticket) => Effect.succeed(ticket)
+      }))
+
+      const request = yield* Effect.async<AsyncIterable<Proto.Flight.FlightData>>((resume, signal) => {
+        resume(Effect.succeed(client.doGet(ticket, { signal })))
+      })
+
+      const reader = yield* Effect.tryPromise({
+        catch: (cause) => new ArrowFlightError({ cause }),
+        try: () =>
+          RecordBatchReader.from({
+            async *[Symbol.asyncIterator]() {
+              for await (const data of request) {
+                // The data length needs to be padded to multiple of 8 bytes.
+                const padding = data.dataBody.length % 8 === 0 ? 0 : 8 - (data.dataBody.length % 8)
+
+                // Create buffer and write metadata prefix.
+                const length = 8 + data.dataHeader.length + padding + data.dataBody.length
+                const buf = new ArrayBuffer(length)
+                const view = new DataView(buf)
+                view.setUint32(0, 0xFFFFFFFF, true) // Continuation token
+                view.setUint32(4, data.dataHeader.length, true) // Header length
+
+                // Copy header and body into buffer.
+                const bytes = new Uint8Array(buf)
+                bytes.set(data.dataHeader, 8)
+                bytes.set(data.dataBody, 8 + data.dataHeader.length + padding)
+
+                yield buf
+              }
+            }
+          } as AsyncIterable<ArrayBuffer>)
+      })
+
+      return Stream.fromAsyncIterable(reader, (cause) => new ArrowFlightError({ cause }))
+    }).pipe(Stream.unwrap)
+
+  const table = (query: string) =>
+    Stream.runCollect(stream(query)).pipe(
+      Effect.map((batches) => new Table(Chunk.toArray(batches)))
+    )
+
+  return { client, stream, table }
+}
+
 export class ArrowFlight extends Effect.Service<ArrowFlight>()("Nozzle/Api/ArrowFlight", {
   dependencies: [FetchHttpClient.layer],
   effect: Effect.gen(function*() {
     const url = yield* Config.string("NOZZLE_ARROW_FLIGHT_URL").pipe(Effect.orDie)
-    const transport = createGrpcTransport({
+    return createGrpcTransport({
       baseUrl: url
     })
-
-    const client = createClient(Proto.Flight.FlightService, transport)
-    const stream = (query: string) =>
-      Effect.gen(function*() {
-        const cmd = create(Proto.FlightSql.CommandStatementQuerySchema, {
-          query
-        })
-
-        // TODO: Why is it necessary to pack the command into an Any?
-        const any = anyPack(Proto.FlightSql.CommandStatementQuerySchema, cmd)
-        const descriptor = create(Proto.Flight.FlightDescriptorSchema, {
-          type: Proto.Flight.FlightDescriptor_DescriptorType.CMD,
-          cmd: toBinary(AnySchema, any)
-        })
-
-        const info = yield* Effect.tryPromise({
-          try: (_) => client.getFlightInfo(descriptor, { signal: _ }),
-          catch: (cause) => new ArrowFlightError({ cause })
-        })
-
-        const ticket = yield* Option.fromNullable(info.endpoint[0]?.ticket).pipe(Option.match({
-          onNone: () => new ArrowFlightError({ cause: new Error("No flight ticket found") }),
-          onSome: (ticket) => Effect.succeed(ticket)
-        }))
-
-        const request = yield* Effect.async<AsyncIterable<Proto.Flight.FlightData>>((resume, signal) => {
-          resume(Effect.succeed(client.doGet(ticket, { signal })))
-        })
-
-        const reader = yield* Effect.tryPromise({
-          catch: (cause) => new ArrowFlightError({ cause }),
-          try: () =>
-            RecordBatchReader.from({
-              async *[Symbol.asyncIterator]() {
-                for await (const data of request) {
-                  // The data length needs to be padded to multiple of 8 bytes.
-                  const padding = data.dataBody.length % 8 === 0 ? 0 : 8 - (data.dataBody.length % 8)
-
-                  // Create buffer and write metadata prefix.
-                  const length = 8 + data.dataHeader.length + padding + data.dataBody.length
-                  const buf = new ArrayBuffer(length)
-                  const view = new DataView(buf)
-                  view.setUint32(0, 0xFFFFFFFF, true) // Continuation token
-                  view.setUint32(4, data.dataHeader.length, true) // Header length
-
-                  // Copy header and body into buffer.
-                  const bytes = new Uint8Array(buf)
-                  bytes.set(data.dataHeader, 8)
-                  bytes.set(data.dataBody, 8 + data.dataHeader.length + padding)
-
-                  yield buf
-                }
-              }
-            } as AsyncIterable<ArrayBuffer>)
-        })
-
-        return Stream.fromAsyncIterable(reader, (cause) => new ArrowFlightError({ cause }))
-      }).pipe(Stream.unwrap)
-
-    const table = (query: string) =>
-      Stream.runCollect(stream(query)).pipe(
-        Effect.map((batches) => new Table(Chunk.toArray(batches)))
-      )
-
-    return { client, stream, table }
-  })
+  }).pipe(Effect.map(make))
 }) {}
+
+export const makeLayer = (transport: Transport) => Layer.sync(ArrowFlight, () => ArrowFlight.make(make(transport)))
+
+export const makeLayerEffect = <E, R>(transport: Effect.Effect<Transport, E, R>) =>
+  transport.pipe(
+    Effect.map(make),
+    Effect.map(ArrowFlight.make),
+    Layer.effect(ArrowFlight)
+  )
+
+export const layerConnect = makeLayerEffect(Effect.gen(function*() {
+  const url = yield* Config.string("NOZZLE_ARROW_FLIGHT_URL").pipe(Effect.orDie)
+  return createConnectTransport({
+    baseUrl: url,
+    httpVersion: "2"
+  })
+}))
 
 export const generateSchema = <T extends TypeMap>(schema: ArrowSchema<T>): Schema.Schema.AnyNoContext => {
   return Schema.Struct(generateFields(schema.fields)) as any
