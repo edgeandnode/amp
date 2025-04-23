c34086a8aca9ee71993c011dc4ac34e2425812a2
Sebastian Lorenz <fubhy@fubhy.com>
2025-04-23 20:01:37 +0200

    fix missing registry dependency

--- typescript/nozzle/src/cli/commands/deploy.ts
+++ typescript/nozzle/src/cli/commands/deploy.ts
@@ -21,6 +21,12 @@ export const deploy = Command.make("deploy", {
     admin: Options.text("admin-url").pipe(
       Options.withFallbackConfig(Config.string("NOZZLE_ADMIN_URL").pipe(Config.withDefault("http://localhost:1610"))),
       Options.withDescription("The url of the Nozzle admin server")
+    ),
+    registry: Options.text("registry-url").pipe(
+      Options.withFallbackConfig(
+        Config.string("NOZZLE_REGISTRY_URL").pipe(Config.withDefault("http://localhost:1611"))
+      ),
+      Options.withDescription("The url of the Nozzle registry server")
     )
   }
 }, ({ args }) =>
@@ -51,7 +57,12 @@ export const deploy = Command.make("deploy", {
     const result = yield* deployer.deploy(manifest)
     yield* Effect.log(result)
   }).pipe(
-    Effect.provide(layer.pipe(Layer.provide(Api.layerAdmin(args.admin))))
+    Effect.provide(
+      layer.pipe(Layer.provide(Layer.mergeAll(
+        Api.layerAdmin(args.admin),
+        Api.layerRegistry(args.registry)
+      )))
+    )
   )).pipe(
     Command.withDescription("Deploy a dataset definition or manifest to Nozzle")
   )
