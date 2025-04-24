c4ff1129fcc616cf219fade68ca977866be3a3fd
Theo Butler <theodusbutler@gmail.com>
2025-04-24 15:35:04 -0400

    use Stream.changes

--- typescript/nozzle/src/EvmRpc.ts
+++ typescript/nozzle/src/EvmRpc.ts
@@ -1,8 +1,8 @@
-import { Context, Effect, Layer, Option, Stream } from "effect"
+import { Context, Effect, Layer, Stream } from "effect"
 import * as Viem from "viem"
 import * as Chains from "viem/chains"
 
-export class EvmRpc extends Context.Tag("Nozzle/EvmRpc")<EvmRpc, ReturnType<typeof make>>() { }
+export class EvmRpc extends Context.Tag("Nozzle/EvmRpc")<EvmRpc, ReturnType<typeof make>>() {}
 
 const make = (url: string) => {
   const rpc = Viem.createPublicClient({
@@ -13,22 +13,20 @@ const make = (url: string) => {
   const watchChainHead = () =>
     Stream.asyncPush<bigint, Error>((emit) =>
       Effect.acquireRelease(
-        Effect.sync(() => {
-          // watchBlockNumber initially returns 0 until the chain progresses,
-          // so we avoid notifying consumers on repeated values.
-          let prevBlock = Option.none<bigint>()
-          return rpc.watchBlockNumber({
-            onBlockNumber: (block) => {
-              if (prevBlock === Option.some(block)) return
-              prevBlock = Option.some(block)
-              emit.single(block)
-            },
+        Effect.sync(() =>
+          rpc.watchBlockNumber({
+            // TODO: this is callback is only called when block numbers are
+            // monotonically increasing
+            onBlockNumber: (block) => emit.single(block),
             onError: (err) => console.error(err.message),
           })
-        }),
+        ),
         (unwatch) => Effect.sync(unwatch),
       )
-    ).pipe(Stream.toPubSub({ capacity: 1, strategy: "sliding" }))
+    ).pipe(
+      Stream.changes,
+      Stream.toPubSub({ capacity: 1, strategy: "sliding" }),
+    )
 
   return { watchChainHead }
 }
