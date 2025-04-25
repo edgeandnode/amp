781a0738ec8c9fd0daa82fefe7db8fe8e77367f0
Sebastian Lorenz <fubhy@fubhy.com>
2025-04-25 12:49:37 +0200

    add typescript ci jobs

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vitest/config"
 
 export default defineConfig({
   test: {
-    workspace: ["typescript/*/vitest.config.ts"]
-  }
+    workspace: ["typescript/*/vitest.config.ts"],
+  },
 })
