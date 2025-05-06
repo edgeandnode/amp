46b47e5f6d8d017cd6b5a7fbe4030e6ee8cebd79
Sebastian Lorenz <fubhy@fubhy.com>
2025-05-06 08:55:52 +0200

    make data dir cleanup uninterruptiple (#235)

--- typescript/nozzle/src/EvmRpc.ts
+++ typescript/nozzle/src/EvmRpc.ts
@@ -1,4 +1,4 @@
-import { Context, Data, Effect, Layer, PubSub, RcRef, Schedule, Stream } from "effect"
+import { Context, Data, Effect, Layer, Option, PubSub, RcRef, Schedule, Stream } from "effect"
 import * as Viem from "viem"
 import * as Chains from "viem/chains"
 
@@ -9,42 +9,67 @@ export class EvmRpc extends Context.Tag("Nozzle/EvmRpc")<EvmRpc, Effect.Effect.S
 }
 
 export class EvmRpcError extends Data.TaggedError("EvmRpcError")<{
-  readonly cause: unknown
+  readonly cause?: unknown
+  readonly message?: string
 }> {}
 
 const make = (url: string) =>
   Effect.gen(function*() {
     const rpc = Viem.createPublicClient({
       chain: Chains.foundry,
-      transport: Viem.http(url),
-      pollingInterval: 1_000,
+      transport: Viem.http(url, { retryCount: 0 }),
     })
 
-    const blocks = yield* RcRef.make({
-      acquire: Stream.asyncPush<bigint, EvmRpcError>((emit) =>
-        Effect.acquireRelease(
-          Effect.sync(() =>
-            rpc.watchBlockNumber({
-              onBlockNumber: (block) => emit.single(block),
-              onError: (error) => emit.fail(new EvmRpcError({ cause: error })),
-              emitMissed: false,
-              emitOnBegin: true,
-            })
-          ),
-          (unwatch) => Effect.sync(unwatch),
-        )
-      ).pipe(
-        Stream.changes,
-        Stream.retry(
-          Schedule.exponential("1 second").pipe(
-            Schedule.jittered,
-            Schedule.union(Schedule.spaced("10 seconds")),
-            Schedule.upTo("1 minute"),
-            Schedule.tapInput(() => Effect.log("Failed to connect to chain. Retrying ...")),
-          ),
+    const latest = Effect.tryPromise({
+      try: () => rpc.getBlockNumber({ cacheTime: 0 }),
+      catch: (cause) => new EvmRpcError({ message: "Failed to get block number", cause }),
+    })
+
+    const block = Effect.fnUntraced(function*(block: bigint) {
+      return yield* Effect.tryPromise({
+        try: () => rpc.getBlock({ blockNumber: block }),
+        catch: (cause) => new EvmRpcError({ message: "Failed to get block", cause }),
+      })
+    })
+
+    const stream = Stream.repeatEffectWithSchedule(latest, Schedule.fixed("1 second")).pipe(
+      Stream.changes,
+      Stream.mapAccumEffect(Option.none<bigint>(), (state, current) => {
+        if (Option.isNone(state)) {
+          return block(current).pipe(Effect.map((block) => [Option.some(current), Stream.succeed(block)]))
+        }
+
+        const range = Stream.range(Number(state.value) + 1, Number(current))
+        const blocks = range.pipe(Stream.mapEffect((number) => block(BigInt(number))))
+        return Effect.succeed([Option.some(current), blocks])
+      }),
+      Stream.flatMap((_) => _),
+      Stream.retry(
+        Schedule.exponential("1 second").pipe(
+          Schedule.jittered,
+          Schedule.union(Schedule.spaced("10 seconds")),
+          Schedule.upTo("1 minute"),
+          Schedule.tapInput(() => Effect.logWarning("Failed to connect to chain. Retrying ...")),
         ),
-        Stream.toPubSub({ capacity: 1, strategy: "sliding" }),
       ),
+      Stream.changesWith((a, b) => a.hash === b.hash),
+      Stream.mapAccumEffect(
+        Option.none<Viem.Block>(),
+        Effect.fnUntraced(function*(previous, current) {
+          // TODO: Implement recovery for this.
+          if (Option.isSome(previous)) {
+            if (previous.value.hash !== current.parentHash) {
+              yield* new EvmRpcError({ message: "Chain reorg detected" })
+            }
+          }
+
+          return [Option.some(current), current]
+        }),
+      ),
+    )
+
+    const blocks = yield* RcRef.make({
+      acquire: Stream.toPubSub(stream, { capacity: "unbounded", replay: 1 }),
       idleTimeToLive: "10 seconds",
     })
 
@@ -53,6 +78,7 @@ const make = (url: string) =>
       Effect.map(Stream.fromQueue),
       Effect.map(Stream.flattenTake),
       Stream.unwrapScoped,
+      Stream.buffer({ capacity: 1, strategy: "sliding" }),
     )
 
     return { url, blocks: watchChainHead }
