5b0bfa4576faa04fcf20b79f4cb4a0d25317c9fd
Sebastian Lorenz <fubhy@fubhy.com>
2025-05-26 17:16:22 +0200

    add build context (#336)

--- typescript/nozzle/src/index.ts
+++ typescript/nozzle/src/index.ts
@@ -1,5 +1,7 @@
-import { Schema } from "effect"
-import * as Model from "./Model.js"
+import type { Context } from "./ConfigLoader.js"
+import type * as Model from "./Model.js"
+
+export type { Context } from "./ConfigLoader.js"
 
 export * as Api from "./Api.js"
 export * as Arrow from "./Arrow.js"
@@ -7,6 +9,6 @@ export * as ArrowFlight from "./ArrowFlight.js"
 export * as JsonLines from "./JsonLines.js"
 export * as Model from "./Model.js"
 
-export const defineDataset = (fn: () => Model.DatasetDefinition) => {
-  return Schema.decodeUnknownSync(Model.DatasetDefinition)(fn())
+export const defineDataset = (fn: (context: Context) => Model.DatasetDefinition) => {
+  return (context: Context) => fn(context)
 }
