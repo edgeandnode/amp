99c1b4ee33a94815b09726ed3de462a0de824c49
John Swanson <jswanson@edgeandnode.com>
2025-06-20 08:01:21 -0700

    chore: remove unused `ArrowError` import
    Signed-off-by: John Swanson <jswanson@edgeandnode.com>

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -24,7 +24,6 @@ pub use catalog::logical::*;
 use datafusion::arrow::{
     array::{ArrayRef, AsArray as _, RecordBatch},
     datatypes::{TimeUnit, UInt64Type, DECIMAL128_MAX_PRECISION},
-    error::ArrowError,
 };
 pub use datafusion::{arrow, parquet};
 use futures::Stream;
