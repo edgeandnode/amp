f7978dd246a79445c1dd0ab5548f9f2572fdcb67
sistemd <enntheprogrammer@gmail.com>
2025-07-11 17:35:56 +0200

    evm-rpc and firehouse support for checking dataset def schema (#453)
    * evm-rpc and firehouse support for checking dataset def schema
    
    * fix tests

--- generate-manifest/src/lib.rs
+++ generate-manifest/src/lib.rs
@@ -1,6 +1,7 @@
 use std::{io, str::FromStr};
 
 use common::BoxError;
+use dataset_store::vec_table_to_schema;
 
 pub fn run<T: io::Write>(
     network: String,
@@ -9,12 +10,25 @@ pub fn run<T: io::Write>(
     w: &mut T,
 ) -> Result<(), BoxError> {
     // Validate dataset kind.
-    let _ = dataset_store::DatasetKind::from_str(&kind)?;
+    let dataset_kind = dataset_store::DatasetKind::from_str(&kind)?;
 
+    let schema = match dataset_kind {
+        dataset_store::DatasetKind::EvmRpc => {
+            Some(vec_table_to_schema(evm_rpc_datasets::tables::all(&network)))
+        }
+        dataset_store::DatasetKind::Firehose => Some(vec_table_to_schema(
+            firehose_datasets::evm::tables::all(&network),
+        )),
+        // TODO
+        dataset_store::DatasetKind::Substreams => todo!(),
+        dataset_store::DatasetKind::Sql => todo!(),
+        dataset_store::DatasetKind::Manifest => todo!(),
+    };
     let dataset = serde_json::to_vec(&dataset_store::DatasetDefsCommon {
         network,
         kind,
         name,
+        schema,
     })?;
     w.write_all(&dataset)?;
 
