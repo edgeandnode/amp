28271212efa8526315bbadaf9cdf28c51802e1d9
sistemd <enntheprogrammer@gmail.com>
2025-08-07 13:04:25 +0200

    fix: wrap jsonl requests in a span

--- server/src/service.rs
+++ server/src/service.rs
@@ -39,6 +39,7 @@ use metadata_db::MetadataDb;
 use prost::Message as _;
 use thiserror::Error;
 use tonic::{Request, Response, Status};
+use tracing::instrument;
 
 type TonicStream<T> = Pin<Box<dyn Stream<Item = Result<T, Status>> + Send + 'static>>;
 
@@ -387,6 +388,7 @@ impl FlightService for Service {
 }
 
 impl Service {
+    #[instrument(skip(self))]
     async fn get_flight_info(&self, descriptor: FlightDescriptor) -> Result<FlightInfo, Error> {
         let (serialized_plan, schema) = match DescriptorType::try_from(descriptor.r#type)
             .map_err(|e| Error::PbDecodeError(e.to_string()))?
@@ -454,6 +456,7 @@ impl Service {
         Ok(info)
     }
 
+    #[instrument(skip(self))]
     async fn do_get(&self, ticket: arrow_flight::Ticket) -> Result<TonicStream<FlightData>, Error> {
         let remote_plan = common::query_context::remote_plan_from_bytes(&ticket.ticket)?;
         let table_refs = remote_plan.table_refs.into_iter().map(|t| t.into());
