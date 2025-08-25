6178fdc942c29c12374a110248573f961e323778
Sebastian Lorenz <fubhy@fubhy.com>
2025-08-25 16:43:20 +0200

    add nix dependencies for `just` and `nextest` (#609)

--- flake.nix
+++ flake.nix
@@ -1,48 +1,50 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    rust.url = "github:oxalica/rust-overlay";
     foundry.url = "github:shazow/foundry.nix/stable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { self, nixpkgs, rust, foundry, ... }:
+  outputs =
+    { nixpkgs, fenix, foundry, ... }:
     let
       overlays = [
-        (import rust)
-        (self: super: {
-          rustToolchain = super.rust-bin.stable.latest.default;
-        })
+        fenix.overlays.default
         foundry.overlay
       ];
 
-      allSystems = [
-        "x86_64-linux" # 64-bit Intel/AMD Linux
-        "aarch64-linux" # 64-bit ARM Linux
-        "x86_64-darwin" # 64-bit Intel macOS
-        "aarch64-darwin" # 64-bit ARM macOS
-      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system: f {
+        toolchain = with fenix.packages.${system}; combine [
+          (fromToolchainFile {
+            file = ./rust-toolchain.toml;
+            sha256 = "sha256-+9FmLhAOezBZCOziO0Qct1NOrfpjNsXxc/8I0c7BdKE=";
+          })
+          stable.rust-src # This is needed for rust-analyzer to find stdlib symbols. Should use the same channel as the toolchain.
+        ];
 
-      # Helper to provide system-specific attributes
-      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
         pkgs = import nixpkgs { inherit overlays system; };
       });
     in
     {
-      devShells = forAllSystems ({ pkgs }: {
+      formatter = forAllSystems ({ pkgs }: pkgs.alejandra);
+      devShells = forAllSystems ({ pkgs, toolchain }: {
         default = pkgs.mkShell {
-          packages = (with pkgs; [
-            # Includes cargo, clippy, cargo-fmt, rustdoc, rustfmt, and other tools.
-            rustToolchain
+          packages = with pkgs; [
+            toolchain
             foundry-bin
             solc
             bun
             protobuf
             uv
             cmake
-            nodejs-slim
+            corepack
+            nodejs
             postgresql
-          ]) ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [
-            libiconv
-          ]);
+            just
+            cargo-nextest
+          ];
         };
       });
     };
