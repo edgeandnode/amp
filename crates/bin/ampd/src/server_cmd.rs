60c27354670e5de9d9d3f5d33b5c7fd9b91e0e91
Lorenzo Delgado <lorenzo@edgeandnode.com>
2025-10-14 00:16:58 +0200

    refactor(server): move serve function to server crate
    Signed-off-by: Lorenzo Delgado <lorenzo@edgeandnode.com>

--- crates/bin/ampd/src/server_cmd.rs
+++ crates/bin/ampd/src/server_cmd.rs
@@ -1,18 +1,7 @@
-use std::{future::Future, net::SocketAddr, sync::Arc};
+use std::sync::Arc;
 
-use arrow_flight::flight_service_server::FlightServiceServer;
-use common::{BoxError, BoxResult, config::Config, utils::shutdown_signal};
-use futures::{FutureExt, TryFutureExt as _};
+use common::{BoxError, config::Config};
 use metadata_db::MetadataDb;
-use server::service::Service;
-use tokio::net::TcpListener;
-use tonic::transport::{Server, server::TcpIncoming};
-
-#[derive(Debug, Clone, Copy)]
-pub struct BoundAddrs {
-    pub flight_addr: SocketAddr,
-    pub jsonl_addr: SocketAddr,
-}
 
 pub async fn run(
     config: Config,
@@ -23,20 +12,6 @@ pub async fn run(
 ) -> Result<(), Error> {
     let config = Arc::new(config);
 
-    let (_, server) = run_servers(config, metadata_db, flight_server, jsonl_server, meter)
-        .await
-        .map_err(Error::ServerStart)?;
-
-    server.await.map_err(Error::ServerRuntime)
-}
-
-pub async fn run_servers(
-    config: Arc<Config>,
-    metadata_db: MetadataDb,
-    enable_flight: bool,
-    enable_jsonl: bool,
-    meter: Option<&monitoring::telemetry::metrics::Meter>,
-) -> Result<(BoundAddrs, impl Future<Output = BoxResult<()>>), BoxError> {
     if config.max_mem_mb == 0 {
         tracing::info!("Memory limit is unlimited");
     } else {
@@ -48,71 +23,18 @@ pub async fn run_servers(
         !config.spill_location.is_empty()
     );
 
-    let service = Service::new(config.clone(), metadata_db.clone(), meter).await?;
+    let (addrs, server) = server::serve(config, metadata_db, flight_server, jsonl_server, meter)
+        .await
+        .map_err(Error::ServerStart)?;
 
-    let mut addrs = BoundAddrs {
-        flight_addr: config.addrs.flight_addr,
-        jsonl_addr: config.addrs.jsonl_addr,
-    };
+    if flight_server {
+        tracing::info!("Arrow Flight RPC server running at {}", addrs.flight_addr);
+    }
+    if jsonl_server {
+        tracing::info!("JSON Lines server running at {}", addrs.jsonl_addr);
+    }
 
-    // Start Arrow Flight Server if enabled
-    let flight_fut = if enable_flight {
-        let listener = TcpListener::bind(config.addrs.flight_addr).await?;
-        let addr = listener.local_addr()?;
-        addrs.flight_addr = addr;
-        tracing::info!("Serving Arrow Flight RPC at {}", addr);
-
-        Server::builder()
-            .add_service(FlightServiceServer::new(service.clone()))
-            .serve_with_incoming_shutdown(
-                TcpIncoming::from_listener(listener, true, None)?,
-                shutdown_signal(),
-            )
-            .map_err(|err| {
-                tracing::error!("Flight server error: {}", err);
-                err.into()
-            })
-            .boxed()
-    } else {
-        Box::pin(std::future::pending())
-    };
-
-    // Start JSON Lines Server if enabled
-    let jsonl_fut = if enable_jsonl {
-        let (jsonl_addr, jsonl_server) =
-            server::jsonl::run_server(service, config.addrs.jsonl_addr).await?;
-        addrs.jsonl_addr = jsonl_addr;
-        tracing::info!("Serving JSON lines at {}", jsonl_addr);
-
-        jsonl_server
-            .map_err(|err| {
-                tracing::error!("JSON lines server error: {}", err);
-                err
-            })
-            .boxed()
-    } else {
-        Box::pin(std::future::pending())
-    };
-
-    // Wait for the services to finish, either due to graceful shutdown or error.
-    let fut = async move {
-        tokio::select! {
-            result = flight_fut => {
-                if let Err(err) = &result {
-                    tracing::error!(error=?err, "Flight server shutting down due to unexpected error");
-                }
-                result
-            }
-            result = jsonl_fut => {
-                if let Err(err) = &result {
-                    tracing::error!(error=?err, "JSONL server shutting down due to unexpected error");
-                }
-                result
-            }
-        }
-    };
-
-    Ok((addrs, fut))
+    server.await.map_err(Error::ServerRuntime)
 }
 
 /// Errors that can occur during server execution.
