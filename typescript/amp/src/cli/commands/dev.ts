0a674acc113793a9c2905132f4a3bd4d45bcce7a
Sebastian Lorenz <fubhy@fubhy.com>
2025-10-15 16:03:36 +0200

    refactor(typescript): simplify dev server to use continuous dumping (#983)

--- typescript/amp/src/cli/commands/dev.ts
+++ typescript/amp/src/cli/commands/dev.ts
@@ -4,14 +4,9 @@ import * as Layer from "effect/Layer"
 import * as Admin from "../../api/Admin.ts"
 import * as ConfigLoader from "../../ConfigLoader.ts"
 import * as DevServer from "../../DevServer.ts"
-import * as EvmRpc from "../../evm/EvmRpc.ts"
 
 export const dev = Command.make("dev", {
   args: {
-    rpcUrl: Options.text("rpc-url").pipe(
-      Options.withDescription("The url of the rpc api to use"),
-      Options.withDefault("http://localhost:8545"),
-    ),
     adminUrl: Options.text("admin-url").pipe(
       Options.withDescription("The url of the admin api to use"),
       Options.withDefault("http://localhost:1610"),
@@ -21,9 +16,6 @@ export const dev = Command.make("dev", {
   Command.withDescription("Run a development server with hot reloading"),
   Command.withHandler(() => DevServer.layer().pipe(Layer.launch)),
   Command.provide(({ args }) =>
-    Layer.mergeAll(
-      ConfigLoader.ConfigLoader.Default.pipe(Layer.provideMerge(Admin.layer(`${args.adminUrl}`))),
-      EvmRpc.layer(`${args.rpcUrl}`),
-    )
+    ConfigLoader.ConfigLoader.Default.pipe(Layer.provideMerge(Admin.layer(`${args.adminUrl}`)))
   ),
 )
