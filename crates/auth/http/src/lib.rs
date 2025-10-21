867de82589a035c8aa2061162491eeddd0e09fb2
Chris Whited <cmwhited1990@gmail.com>
2025-10-21 11:15:08 -1000

    feat(auth http): unnecessary full path qualification

--- crates/auth/http/src/lib.rs
+++ crates/auth/http/src/lib.rs
@@ -39,7 +39,7 @@ pub use privy_rs::generated::types::{
 ///     Ok(Json(result))
 /// }
 /// ```
-#[derive(Debug, thiserror::Error)]
+#[derive(Debug, Error)]
 #[error(transparent)]
 pub struct AuthAnyhowError(anyhow::Error);
 
