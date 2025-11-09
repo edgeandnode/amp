2ce4a91d64acf94990485cf5ebd3fce7703a3682
Sebastian Lorenz <fubhy@fubhy.com>
2025-11-09 19:15:48 +0100

    fix(core): dump derived dataset tables in parallel (#1184)

--- tests/datasets/sql_over_anvil_1/amp.config.ts
+++ tests/datasets/sql_over_anvil_1/amp.config.ts
@@ -12,6 +12,16 @@ export default defineDataset(() => ({
 from anvil_rpc.blocks`,
       network: "anvil",
     },
+    transactions: {
+      sql: `select block_num, tx_hash, tx_index
+from anvil_rpc.transactions`,
+      network: "anvil",
+    },
+    logs: {
+      sql: `select block_num, tx_hash, log_index
+from anvil_rpc.logs`,
+      network: "anvil",
+    },
   },
   functions: {},
 }))
