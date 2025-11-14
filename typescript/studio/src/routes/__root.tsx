9e0c916b98b1d077897b00b8f67c5bcb752698d1
ak <a.kemalakcay@gmail.com>
2025-11-14 14:44:41 +0300

    use GDSProvider

--- typescript/studio/src/routes/__root.tsx
+++ typescript/studio/src/routes/__root.tsx
@@ -1,4 +1,5 @@
 import { Toast } from "@base-ui-components/react/toast"
+import { GDSProvider } from "@graphprotocol/gds-react"
 import type { QueryClient } from "@tanstack/react-query"
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router"
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools"
@@ -10,12 +11,12 @@ export interface DatasetWorksRouterCtx {
 export const Route = createRootRouteWithContext<DatasetWorksRouterCtx>()({
   component() {
     return (
-      <div className="flex flex-col h-full min-h-screen w-screen">
+      <GDSProvider>
         <Toast.Provider>
           <Outlet />
           <TanStackRouterDevtools />
         </Toast.Provider>
-      </div>
+      </GDSProvider>
     )
   },
 })
