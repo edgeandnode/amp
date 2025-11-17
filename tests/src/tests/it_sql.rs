d763005923b4a9644dfebb9a5411bfefd66d873b
sistemd <enntheprogrammer@gmail.com>
2025-11-17 18:08:27 +0100

    test(udfs): add a regression test for `EvmDecodeParams`
    - Add a regression test for the scenario where we attempt to decode from
      empty/too short data.
    - For the snapshot we are using a block that is known to have caused a
      panic when running a query that uses `EvmDecodeParams`.

--- tests/src/tests/it_sql.rs
+++ tests/src/tests/it_sql.rs
@@ -43,3 +43,24 @@ async fn sql_advanced_tests() {
         .await
         .expect("Failed to run spec");
 }
+
+#[tokio::test]
+async fn sql_regression_tests() {
+    logging::init();
+
+    let test_ctx = TestCtxBuilder::new("sql_regression_tests")
+        .with_dataset_manifests(["eth_firehose_evm_decode_empty"])
+        .with_dataset_snapshots(["eth_firehose_evm_decode_empty"])
+        .with_provider_configs(["firehose_eth_mainnet"])
+        .build()
+        .await
+        .expect("Failed to create test environment");
+    let mut client = test_ctx
+        .new_flight_client()
+        .await
+        .expect("Failed to connect FlightClient");
+
+    run_spec("sql-regression-tests", &test_ctx, &mut client, None)
+        .await
+        .expect("Failed to run spec");
+}
