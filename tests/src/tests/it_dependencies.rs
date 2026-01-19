ae55957f3a7294a7ca8dbf365e3107754f28c29f
Shiyas Mohammed <shiyasmohd.official@gmail.com>
2026-01-19 12:26:20 +0530

    chore(tests): remove unnecessary delay in tests

--- tests/src/tests/it_dependencies.rs
+++ tests/src/tests/it_dependencies.rs
@@ -21,14 +21,9 @@ async fn intra_deps_test() {
         .await
         .expect("Failed to connect FlightClient");
 
-    run_spec(
-        "intra-deps",
-        &test_ctx,
-        &mut client,
-        Some(Duration::from_millis(100)),
-    )
-    .await
-    .expect("Failed to run spec");
+    run_spec("intra-deps", &test_ctx, &mut client, None)
+        .await
+        .expect("Failed to run spec");
 }
 
 #[tokio::test]
@@ -47,12 +42,7 @@ async fn multi_version_test() {
         .await
         .expect("Failed to connect FlightClient");
 
-    run_spec(
-        "multi-version",
-        &test_ctx,
-        &mut client,
-        Some(Duration::from_millis(100)),
-    )
-    .await
-    .expect("Failed to run spec");
+    run_spec("multi-version", &test_ctx, &mut client, None)
+        .await
+        .expect("Failed to run spec");
 }
