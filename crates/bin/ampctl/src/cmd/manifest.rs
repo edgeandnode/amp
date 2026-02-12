5b49a773b18dcd44245cdff2f77433a2b62d8969
Theo Butler <theodusbutler@gmail.com>
2026-02-12 08:17:36 -0700

    fix(ampctl): make list command aliases consistent

--- crates/bin/ampctl/src/cmd/manifest.rs
+++ crates/bin/ampctl/src/cmd/manifest.rs
@@ -24,6 +24,7 @@ pub enum Commands {
     Register(register::Args),
 
     /// List all registered manifests
+    #[command(alias = "ls")]
     #[command(after_help = include_str!("manifest/list__after_help.md"))]
     List(list::Args),
 
