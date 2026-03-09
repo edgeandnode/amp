cdc5de92ee1b4350edf727431ed0d42ef6d37764
Leonardo Yvens <leoyvens@gmail.com>
2026-03-09 13:10:30 +0000

    chore(common): fmt

--- crates/core/common/src/catalog/physical/snapshot.rs
+++ crates/core/common/src/catalog/physical/snapshot.rs
@@ -251,12 +251,7 @@ impl QueryableSnapshot {
         let partitioned = round_robin(file_stream, file_count, target_partitions)
             .await
             .map_err(|e| DataFusionError::External(e.into()))?;
-        compute_all_files_statistics(
-            partitioned,
-            table_schema,
-            true,
-            false,
-        )
+        compute_all_files_statistics(partitioned, table_schema, true, false)
     }
 }
 
